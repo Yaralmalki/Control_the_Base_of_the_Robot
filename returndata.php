6d4c89e52d66786f28ee8e1ac1783a274664d3e2
Yaralmalki <71742682+Yaralmalki@users.noreply.github.com>
2021-06-29 13:47:48 -0700

    Update returndata.php

--- returndata.php
+++ returndata.php
@@ -9,11 +9,11 @@ if (mysqli_num_rows($sqli) > 0) {
    $row = mysqli_fetch_array($sqli); 
 //Print the last values inserted 
  //  echo "  "  . $row["id"] . "<br>";
-    echo " " .   $row["forward"]; 
-    echo " " .   $row["stop"]; 
-    echo " " .   $row["lef"]; 
-    echo " " .   $row["righ"];
-    echo " " .   $row["backward"]; 
+    echo "Direction " .   $row["forward"]; 
+    echo "Direction " .   $row["stop"]; 
+    echo "Direction " .   $row["lef"]; 
+    echo "Direction " .   $row["righ"];
+    echo "Direction " .   $row["backward"]; 
 }
 else{
     echo "No Result Found";
